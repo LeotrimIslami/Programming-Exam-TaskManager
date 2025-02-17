b0f89d3c18693818f414e4bab9b8cf4fefc89c3a
LeotrimIslami <leotrimislami98@gmail.com>
2025-02-17 15:28:01 +0100

    Feat: Shtova tasken TaskItem

---
+++ TaskManager/TaskItem.cs
@@ -0,0 +1,8 @@
+﻿public class TskItem
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+    public string Category { get; set; }
+}
